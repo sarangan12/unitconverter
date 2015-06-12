208bd981df0c4dfcfb76448133a886712a100cc4
Sarangan Rajamanickam <sarangan.rajamanickam@gmail.com>
2015-06-12 15:45:24 -0700

    Added Angle Engine
    Added Angle Engine

--- convertermain.js
+++ convertermain.js
@@ -64,8 +64,56 @@ app.controller('myCtrl', function($scope) {
 	    $scope.toValue = digitalStorageEngine.Convert();
         return;
 	  }
+
+	  if($scope.subcategory === "Angle") {
+	    var angleEngine = new AngleEngine(parseFloat($scope.fromValue), $scope.fromId, $scope.toId);
+	    $scope.toValue = angleEngine.Convert();
+	    return;
+	  }
     }
 });
+
+function AngleEngine(fromValue, fromId, toId){
+  this.toId = toId;
+  this.fromId = fromId;
+  this.fromValue = fromValue;
+}
+
+AngleEngine.prototype.DegreeToRadian = function(fv) {
+  return (fv * 0.0174532925);
+}
+
+AngleEngine.prototype.RadianToRadian = function(fv) {
+  return fv;
+}
+
+AngleEngine.prototype.RadianToDegree = function(fv) {
+  return (fv / 0.0174532925);
+}
+
+AngleEngine.prototype.Convert = function(){
+  var intermediateRadianValue;
+  switch (this.fromId) {
+    case "Degree":
+      intermediateRadianValue = this.DegreeToRadian(this.fromValue);
+      break;
+    case "Radian":
+      intermediateRadianValue = this.RadianToRadian(this.fromValue);
+      break;
+  }
+
+  var finalValue;
+  switch(this.toId){
+    case "Degree":
+      finalValue = this.RadianToDegree(intermediateRadianValue);
+      break;
+    case "Radian":
+      finalValue = this.RadianToRadian(intermediateRadianValue)
+      break;
+  }
+
+  return finalValue;
+}
 
 function DigitalStorageEngine(fromValue, fromId, toId){
   this.toId = toId;
